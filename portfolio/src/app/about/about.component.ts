f866d67c61f61e56e64c3683813b2f65f063f2cb
Chalisa Keaw Phoomsakha <44123218+chphoom@users.noreply.github.com>
2024-03-19 18:42:58 -0400

    use variables

--- portfolio/src/app/about/about.component.ts
+++ portfolio/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  public p1: string;
+  public p2: string;
+  // TODO: add image the same way
 
+  constructor(private data: DataService){
+    let temp = data.getAboutMe()
+    this.p1 = temp[0]
+    this.p2 = temp[1]
+  }
 }
--- portfolio/src/app/home/home.component.ts
+++ portfolio/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  public name: string;
+  public p1: string;
+  public p2: string;
+  public email: string;
 
+  constructor(private data: DataService){
+    this.name = data.getName()
+    let bio = data.getBio()
+    this.p1 = bio[0]
+    this.p2 = bio[1]
+    this.email = data.getEmail()
+  }
 }
