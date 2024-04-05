a8b9a138a98df7287ce7e7507a825189c7a904f2
Chalisa Keaw Phoomsakha <44123218+chphoom@users.noreply.github.com>
2024-04-05 14:10:11 -0400

    tablet mode

--- portfolio/src/app/other-projects/other-projects.component.ts
+++ portfolio/src/app/other-projects/other-projects.component.ts
@@ -11,14 +11,17 @@ export class OtherProjectsComponent {
   public other: Project[];
   public expanded: boolean;
   public length: number;
+  public buttonLabel: string;
 
   constructor(private data: ProjectService){
     this.other = data.getOtherProjects()
     this.expanded = false
     this.length = this.other.length
+    this.buttonLabel = this.expanded ? "Show Less" : "Show More"
   }
 
   onClick(){
     this.expanded = !this.expanded
+    this.buttonLabel = this.expanded ? "Show Less" : "Show More"
   }
 }
