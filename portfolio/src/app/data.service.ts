904cd5eed49ba3adf546bfc34e98e685a6ed6df4
Chalisa Keaw Phoomsakha <44123218+chphoom@users.noreply.github.com>
2024-10-01 18:12:31 -0400

    update content

--- portfolio/src/app/data.service.ts
+++ portfolio/src/app/data.service.ts
@@ -24,12 +24,12 @@ export class DataService {
 
   // these are arrays because of paragraph seperation
   homeBio: string[] = [
-    "As a recent Computer Science graduate, I'm thrilled to embark on my journey in software engineering. With a passion for both web and app development, I bring a curious and adaptable mindset to every project. Whether it's exploring new technologies or solving complex problems, I'm eager to dive in and make meaningful contributions.",
-    "Having completed my degree, I'm now seeking opportunities to apply my skills in a dynamic and collaborative environment. I'm excited to join teams where I can continue to learn, grow, and contribute to innovative projects. Let's connect and see how we can shape the future together!"
+    "As a Master’s student in Computer Science specializing in Interactive Intelligence, I’m excited to deepen my expertise in the fields of AI and human-computer interaction. With a passion for both web and app development, I bring a curious and adaptable mindset to every project. I thrive on exploring new technologies and solving complex problems, and I’m eager to apply my growing knowledge in innovative ways.",
+    "Currently pursuing my Master’s part-time, I’m actively seeking full-time positions where I can contribute to impactful projects while continuing my academic journey. I’m also looking to join a research group focused on advancing interactive intelligence. Let’s connect and explore how we can shape the future together!"
   ]
 
   aboutme: string[] = [
-    "Hey there! I stumbled into programming through quirky avenues like the Episode App Writers Portal, Tumblr blogs, and Minecraft mods. It sparked a fire in me for coding, leading me to pursue a Bachelor's in Computer Science at the University of North Carolina at Chapel Hill (2019-2023), where I graduated with a GPA of 3.0/4.0.",
+    "Hey there! I stumbled into programming through quirky avenues like the Episode App Writers Portal, Tumblr blogs, and Minecraft mods. It sparked a fire in me for coding, leading me to pursue a Bachelor's in Computer Science at the University of North Carolina at Chapel Hill (2019-2023).  Now, I’m continuing that journey by pursuing a Master’s in Computer Science at the Georgia Institute of Technology, with a focus on Interactive Intelligence.",
     "Outside of class, I'm all about diving deeper into programming, especially AI and machine learning. Whether it's tinkering with new libraries or joining coding communities, I'm always hungry to learn more.",
     "Along the way, I've dabbled in various technologies, leading projects like Stayfull and ShiftOverflow. Let's team up, brainstorm cool ideas, and push the boundaries of what's possible in tech. Here's a glimpse of some of the skills I've picked up along the journey:"
   ]
